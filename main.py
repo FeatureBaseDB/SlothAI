610d8ac0ac8861b6c78af26fbfbf4bea9596f0d4
Kord Campbell <kordless@gmail.com>
2023-08-25 12:41:50 -0500

    blueprints, deploy and dev scripts

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from flask import Flask
 
+from flask import Flask, session, request, render_template, make_response, redirect, url_for
+
+from web.site import site
+
+import config
+
+# create app
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
+app.register_blueprint(site)
 
-@app.route('/')
-def main():
-    return 'Hello world.'
+@app.errorhandler(404)
+def f404_notfound(e):
+    response = make_response(
+        render_template(
+            'pages/404_notfound.html'
+        )
+    )
+    return response, 404
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    dev = True
 
---
+++ web/site.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from flask import Blueprint, render_template, flash
+from flask import make_response, Response
+from flask import redirect, url_for, abort
+from flask import request, send_file
+
+site = Blueprint('site', __name__)
+
+import config
+
+@site.route('/sitemap.txt')
+def sitemap():
+	return render_template('pages/sitemap.txt')
+
+# main route
+@site.route('/')
+def main():
+    return 'Hello world.'
