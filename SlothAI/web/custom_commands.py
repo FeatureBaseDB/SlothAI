05508ddf2ef36506440c9a02ffdca1cfac795cf9
Kord Campbell <kordless@gmail.com>
2023-11-02 18:43:09 -0500

    overlap and nltk issue

--- SlothAI/web/custom_commands.py
+++ SlothAI/web/custom_commands.py
@@ -2,12 +2,12 @@ from flask import Blueprint, render_template
 from flask import current_app as app
 from faker import Faker
 
+import zlib
+
 import nltk
 from nltk.data import find
-nltk.download('punkt')
-
-# Load the Punkt tokenizer
-tokenizer = nltk.data.load(find('tokenizers/punkt/english.pickle'))
+nltk.data.load('./SlothAI/static/english.pickle')
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 custom_commands = Blueprint('custom_commands', __name__)
 
@@ -29,70 +29,120 @@ def random_sentence():
 
 
 @custom_commands.app_template_global()
-def chunk_with_page_filename(texts, filename, overlap=0):
+def chunk_with_page_filename(texts, filename, length=512, overlap=0):
+    # main object
+    texts_chunks = []
+
+    # loop over page's texts
+    for text in texts:
+        # lists for page_chunks and sub_chunks
+        page_chunks = []
+        sub_chunks = []
+
+        # Loop over the tokenized text for the page
+        for i, entry in enumerate(tokenizer.tokenize(text)):
+            if entry.strip():
+                # Remove extra whitespace and newline characters
+                entry = entry.replace("\n"," ").replace("\r"," ").replace("\t"," ").replace('"','``').replace("'","`").replace("\\"," ").strip()
+
+                if not i and len(' '.join(sub_chunks)) + len(entry) > length:
+                     sub_chunks.append(entry)
+                     sub_chunks = []
+
+                # if current chunks + new entry's length is > length, we append to the chunks
+                elif len(' '.join(sub_chunks)) + len(entry) > length:
+                    # Append the current segment to the list
+                    page_chunks.append(sub_chunks)
+                    sub_chunks = []
+
+                # Add the entry to the current chunks
+                sub_chunks.append(entry)
+
+        # Append the last segment to the list if it's not empty
+        if sub_chunks:
+            page_chunks.append(sub_chunks)
+
+        if page_chunks:
+            texts_chunks.append(page_chunks)
+
+    if overlap:
+        texts_chunks_copy = []
+        for texts_chunk in texts_chunks:
+            for i in range(1, len(texts_chunk)):
+                prev_last_strings = texts_chunk[i-1][-overlap:]
+                current_first_strings = texts_chunk[i][:overlap]
+     
+                for string in current_first_strings:
+                    texts_chunk[i-1].append(string)
+     
+
+                for string in prev_last_strings:
+                    texts_chunk[i].insert(0, string)
+
+            texts_chunks_copy.append(texts_chunk)
+
+        texts_chunks = texts_chunks_copy
+
+        # Loop over the pages, starting from the second page (index 1)
+        for i in range(1, len(texts_chunks)):
+            # Find the last `overlap` strings from the previous page
+            prev_last_strings = find_last_strings(texts_chunks[i-1], num=overlap)
+            
+            # Find the first `overlap` strings from the current page
+            current_first_strings = find_first_strings(texts_chunks[i], num=overlap)
+            
+            # Prepend the last strings of the previous page to the beginning of the current page
+            texts_chunks[i][0] = prev_last_strings + texts_chunks[i][0]
+            
+            # Append the first strings of the current page to the end of the last chunk of the previous page
+            texts_chunks[i-1][-1] = texts_chunks[i-1][-1] + current_first_strings
+
     segmented_texts = []
     page_numbers = []
     chunk_numbers = []
     filenames = []
 
-    current_chunk_number = 1
     current_page_number = 1
-    current_segment = ""
+    current_chunk_number = 1
 
-    for text in texts:
-        # Initialize an empty list to store text segments for this text
-        segments = []
+    for text in texts_chunks:
+        for te in text:
+            segmented_texts.append(' '.join(te))
+            page_numbers.append(current_page_number)
+            chunk_numbers.append(current_chunk_number)
+            filenames.append(filename)
 
-        # Loop over the tokenized text
-        for entry in tokenizer.tokenize(text):
-            # Remove extra whitespace and newline characters
-            entry = entry.strip().replace("\n"," ").replace("\r"," ").replace("\t"," ").replace('"','``').replace("'","`").replace("\\"," ")
+            current_chunk_number = current_chunk_number + 1
 
-            # Check if adding this entry to the current segment will exceed 512 characters
-            if len(current_segment) + len(entry) > 512:
-                # Append the current segment to the list
-                segments.append(current_segment)
-                current_segment = ""
+        current_page_number = current_page_number + 1
 
-            # Add the entry to the current segment
-            current_segment += " " + entry
-
-        # Append the last segment to the list if it's not empty
-        if current_segment:
-            segments.append(current_segment)
-
-        # Combine overlapping segments based on the specified overlap parameter
-        if overlap > 0:
-            overlapped_segments = []
-            for i in range(len(segments)):
-                segment = segments[i]
-                if i > 0:
-                    # Combine with the last segment, considering the overlap
-                    combined_segment = segments[i - 1][-overlap:] + segment
-                    overlapped_segments.append(combined_segment)
-                else:
-                    overlapped_segments.append(segment)
-
-            segments = overlapped_segments
-
-        # Add the segmented text for this input to the final list
-        segmented_texts.extend(segments)
-
-        # Add page numbers, chunk numbers, and filenames
-        page_numbers.extend([current_page_number] * len(segments))
-        chunk_numbers.extend(list(range(current_chunk_number, current_chunk_number + len(segments))))
-        filenames.extend([filename] * len(segments))
-
-        # Update page and chunk numbers for the next text
-        current_chunk_number += len(segments)
-        current_page_number += 1
-
-    return {
+    return({
         "chunks": segmented_texts,
         "page_nums": page_numbers,
         "chunk_nums": chunk_numbers,
         "filenames": filenames
-    }
+    })
 
+def find_last_strings(data, num):
+    last_strings = []
+    def recursive_traverse(data):
+        for item in data:
+            if isinstance(item, list):
+                recursive_traverse(item)
+            elif isinstance(item, str):
+                last_strings.append(item)
+    recursive_traverse(data)
+    return last_strings[-num:]
 
-
+def find_first_strings(data, num):
+    first_strings = []
+    def recursive_traverse(data):
+        for item in data:
+            if isinstance(item, list):
+                recursive_traverse(item)
+            elif isinstance(item, str):
+                first_strings.append(item)
+                if len(first_strings) >= num:
+                    return
+    recursive_traverse(data)
+    return first_strings[:num]
